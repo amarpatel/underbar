32e570e36ede0eb0d30cadd4fe35086abd9890d8
Amar Patel <theamarpatel@gmail.com>
2014-01-23 19:23:27 -0800

    Completed last

--- src/underbar.js
+++ src/underbar.js
@@ -29,6 +29,19 @@ var _ = { };
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    if (n === undefined) {
+      return array[array.length-1];
+    } else if (n === 0) {
+      return [];
+    } else if (n > array.length) {
+      return array;
+    } else {
+        var result = [];
+        for (var i=array.length-n;i<array.length;i++) {
+            result.push(array[i]);
+        }
+        return result;
+    }
   };
 
   // Call iterator(value, key, collection) for each element of collection.
