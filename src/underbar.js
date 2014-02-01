7e39cb4d3e26b5bc3dcce486453fd96fd5d290f2
Amar Patel <theamarpatel@gmail.com>
2014-01-31 18:39:46 -0800

    Completed memoize

--- src/underbar.js
+++ src/underbar.js
@@ -301,6 +301,12 @@ var _ = { };
   // already computed the result for the given argument and return that value
   // instead if possible.
   _.memoize = function(func) {
+    var memo = {}, hasher;
+    (arguments[length-1] === undefined) ? hasher = _.identity : hasher = arguments[1];
+    return function() {
+      var key = hasher.apply(this,arguments);
+      return Object.prototype.hasOwnProperty.call(memo,key) ? memo[key] : (memo[key] = func.apply(this, arguments));
+    };
   };
 
   // Delays a function for the given number of milliseconds, and then calls
